a68fb770dc38cbacb060ab2a5d63a2942afc1399
Relyful <13381270+Relyful@users.noreply.github.com>
2025-08-27 21:23:53 +0200

    Abort fetch on reload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,11 @@ import { Link } from 'react-router'
 function App() {
   const [posts, setPosts] = useState([{title: 'My first post', message: 'body of my first post wow', createdAt: 'some time', comments: [], author: 'Rely'}]);
   useEffect( () => {
+    const controller = new AbortController();
+    const signal = controller.signal;
     const fetchPosts = async () => {
       try{
-        const response = await fetch('http://localhost:8080/posts');
+        const response = await fetch('http://localhost:8080/posts', { signal });
         if (!response.ok) {
           throw new Error('Fetch error');
         };
@@ -16,16 +18,21 @@ function App() {
         console.log(postsData);
         setPosts(postsData);
       } catch (err) {
-        console.log(err);
+        if (err.name === 'AbortError') {
+          console.log('Request aborted');
+        } else {
+          console.log(err);
+        }
       }
       
 
     }
     fetchPosts();
+    return () => controller.abort();
   }, []);
 
   const postItems = posts.map(post => {
-    return <div className="post" key={post}>
+    return <div className={`post ${styles.post}`} key={post.id}>
       <div className="title">{post.title}</div>
       <div className="message">{post.message}</div>
       <div className="createdAt">{post.createdAt}</div>
