2ae2dbec0f7f777b239e5d45c6eee3df34ab7835
Relyful <13381270+Relyful@users.noreply.github.com>
2025-08-28 22:25:09 +0200

    Create form that requests data from api and returns jwt

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,7 +1,37 @@
+import styles from "./LogIn.module.css"
+
 export default function LogIn() {
+
+  async function handleLogIn(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get('username');
+    const password = formData.get('password');
+    console.log('E target');
+    console.log(e.target);
+    try {
+      const requestBody = {username, password};
+      const response = await fetch("http://localhost:8080/login", {method: 'POST', body: JSON.stringify(requestBody), headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}});
+      if (!response.ok) {
+        throw new Error('fetch error'); 
+      };
+      const responseData = await response.json();
+      console.log(responseData)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
 return (
   <>
     <h2>This is a login form</h2>
+    <form action="" method="post" onSubmit={handleLogIn} className={`${styles.form}`}>
+      <label htmlFor="username">Username: </label>
+      <input type="text" name="username" id="username" />
+      <label htmlFor="password">Password: </label>
+      <input type="password" name="password" id="password" />
+      <button type="submit">Submit</button>
+    </form>
   </>
 )
 }
