db066fd67a8801ba7576bbf23455f3d93b5ad267
Relyful <13381270+Relyful@users.noreply.github.com>
2025-09-02 21:40:30 +0200

    When logged in show username instead login link in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,63 @@
-import styles from "./Header.module.css"
-import { Link, Outlet } from 'react-router'
+import styles from "./Header.module.css";
+import { Link, Outlet } from "react-router";
+import { useEffect, useState } from "react";
 
-function App() {  
+function App() {
+  const [user, setUser] = useState({});
+  const jwt = localStorage.getItem("authToken");
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    if (jwt) {
+      async function authUser() {
+        try {
+          const response = await fetch("http://localhost:8080/auth", {
+            signal,
+            headers: {
+              Authorization: `Bearer ${jwt}`,
+            },
+          });
+          if (!response.ok) {
+            localStorage.removeItem("authToken");
+            setUser(undefined)         
+            throw new Error("Auth failed");
+          }
+          const data = await response.json();
+          console.log(data.user);
+          setUser(data.user);
+        } catch (err) {
+          if (err.name === "AbortError") {
+            console.log("Request aborted");
+          } else {
+            console.log(err);
+          }
+        }
+      }
+      console.log("hehtest");
+      authUser();
+    }
+    return () => controller.abort();
+  }, [jwt]);
+
   return (
-  <>
-   <header className={styles.header}>
-    <h1 className="logo">Rely's Blog</h1>
-    <div className={`links ${styles.headerLinks}`}>
-      <Link to="/">Home</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/login">Log In</Link>
-    </div>
-   </header>
-   <main>
-    <Outlet />
-   </main>
-   </>
-  )
+    <>
+      <header className={styles.header}>
+        <h1 className="logo">Rely's Blog</h1>
+        <div className={`links ${styles.headerLinks}`}>
+          <Link to="/">Home</Link>
+          <Link to="/posts">Posts</Link>
+          {jwt ? (
+            <span>Username: {user.username}</span>
+          ) : (
+            <Link to="/login">Log In</Link>
+          )}
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
