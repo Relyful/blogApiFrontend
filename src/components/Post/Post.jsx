e5e0a38b5daeab719c41dfa863d63adada426e27
Relyful <13381270+Relyful@users.noreply.github.com>
2025-09-08 16:22:19 +0200

    Add post component and add logic to fetch and show post data

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,55 @@
+// import styles from "./Post.module.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+
+export default function Post() {
+  const { postId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+      const controller = new AbortController();
+      const signal = controller.signal;
+      const jwt = localStorage.getItem("authToken");
+      const fetchPost = async () => {
+        try {
+          const response = await fetch(`http://localhost:8080/posts/${postId}`, { signal, 
+            headers: {
+              Authorization: `Bearer ${jwt}`,
+            }, });
+          if (!response.ok) {
+            throw new Error("Fetch error");
+          }
+          console.log(response);
+          const postData = await response.json();
+          console.log(postData);
+          setPost(postData)
+          setLoading(false);
+        } catch (err) {
+          if (err.name === "AbortError") {
+            console.log("Request aborted");
+          } else {
+            console.log(err);
+          }
+        }
+      };
+      fetchPost();
+      return () => controller.abort();
+    }, [postId]);
+    
+ if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!post) {
+    return <p>Post not found.</p>;
+  }
+
+  return (
+    <div className="post">
+      <div className="title">{post.title}</div>
+      <div className="content">{post.message}</div>
+      <div className="comments">Comments will be here</div>
+    </div>
+  );
+}
