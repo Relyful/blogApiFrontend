7b1d9f36b7ba1d09f63985560ca0fc3830f602d4
Relyful <13381270+Relyful@users.noreply.github.com>
2025-10-27 20:52:25 +0100

    Style login page

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,47 +1,58 @@
 import { useNavigate, useOutletContext } from "react-router";
-import styles from "./LogIn.module.css"
+import styles from "./LogIn.module.css";
 import { useState } from "react";
 
 export default function LogIn() {
   const navigate = useNavigate();
-  const [ err, setErr ] = useState();
-  const {setUser} = useOutletContext();
+  const [err, setErr] = useState();
+  const { setUser } = useOutletContext();
 
   async function handleLogIn(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const username = formData.get('username');
-    const password = formData.get('password');
+    const username = formData.get("username");
+    const password = formData.get("password");
     try {
-      const requestBody = {username, password};
-      const response = await fetch("http://localhost:8080/login", {method: 'POST', body: JSON.stringify(requestBody), headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}});
+      const requestBody = { username, password };
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
       if (response.status === 401 || response.status === 400) {
-         setErr('Incorrect username of password');
-         throw new Error('Login failed');
+        setErr("Incorrect username of password");
+        throw new Error("Login failed");
       } else if (!response.ok) {
-        throw new Error('Login Server Error');
+        throw new Error("Login Server Error");
       }
       const responseData = await response.json();
       console.log(responseData);
       setUser(responseData.userData);
       localStorage.setItem("authToken", responseData.token);
-      navigate('/');
+      navigate("/");
     } catch (err) {
       console.error(err);
     }
   }
 
-return (
-  <>
-    <h2>This is a login form</h2>
-    <form action="" method="post" onSubmit={handleLogIn} className={`${styles.form}`}>
-      <label htmlFor="username">Username: </label>
-      <input type="text" name="username" id="username" />
-      <label htmlFor="password">Password: </label>
-      <input type="password" name="password" id="password" />
-      <button type="submit">Submit</button>
-    </form>
-    <p>{err}</p>
-  </>
-)
+  return (
+    <div className={styles.login}>
+      <div className={styles.loginContent}>
+        <h2>Log In</h2>
+        <form className={styles.mainForm} onSubmit={handleLogIn}>
+          <label htmlFor="username">Username: </label>
+          <input type="text" name="username" id="username" />
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" />
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
+        </form>
+        <p className={styles.error}>{err}</p>
+      </div>
+    </div>
+  );
 }
