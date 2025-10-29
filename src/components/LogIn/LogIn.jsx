4efb37b9ac358425ecd92903a3a9e525599dafc3
Relyful <13381270+Relyful@users.noreply.github.com>
2025-10-29 21:02:18 +0100

    Return to previous page upon login

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -32,7 +32,7 @@ export default function LogIn() {
       console.log(responseData);
       setUser(responseData.userData);
       localStorage.setItem("authToken", responseData.token);
-      navigate("/");
+      navigate(-1);
     } catch (err) {
       console.error(err);
     }
