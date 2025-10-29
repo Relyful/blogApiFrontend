a7dea4a0d715087a8cbeaf85279387a32a137825
Relyful <13381270+Relyful@users.noreply.github.com>
2025-10-29 20:48:20 +0100

    Show new comment immediately upon posting

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,57 +1,72 @@
 // import styles from "./Post.module.css";
 import { useState, useEffect } from "react";
 import { useOutletContext, useParams } from "react-router";
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
 function Comments({ commentsData }) {
-  const commentsResult = commentsData.map(comment => {
+  const commentsResult = commentsData.map((comment) => {
     return (
       <div className={`comment ${styles.comment}`} key={comment.id}>
-        <p className={styles.commentTitle}>{comment.author.username} said on {new Date(comment.createdAt).toLocaleDateString(undefined, {
+        <p className={styles.commentTitle}>
+          {comment.author.username} said on{" "}
+          {new Date(comment.createdAt).toLocaleDateString(undefined, {
             day: "numeric",
             month: "long",
             year: "numeric",
             hour: "numeric",
-            minute: "numeric"
-          })}: </p>
+            minute: "numeric",
+          })}
+          :{" "}
+        </p>
         <p className={styles.commentMessage}>{comment.message}</p>
       </div>
-    )
+    );
   });
   return <>{commentsResult}</>;
 }
 
-function NewCommentForm({ postId }) {
-
+function NewCommentForm({ postId, onNewComment }) {
   async function handleNewComment(e) {
-    console.log({'works': e});
+    console.log({ works: e });
     const jwt = localStorage.getItem("authToken");
     e.preventDefault();
     const formData = new FormData(e.target);
-    const newComment = formData.get('newComment');
+    const newComment = formData.get("newComment");
     try {
-      const requestBody = {'message': newComment};
-      const response = await fetch(`http://localhost:8080/posts/${postId}/comments`, {
-        method: 'POST', 
-        body: JSON.stringify(requestBody), 
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-          'Content-Type': 'application/json',
-        }
-      });
+      const requestBody = { message: newComment };
+      const response = await fetch(
+        `http://localhost:8080/posts/${postId}/comments`,
+        {
+          method: "POST",
+          body: JSON.stringify(requestBody),
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+            "Content-Type": "application/json",
+          },
+        },
+      );
       if (!response.ok) {
-        throw new Error('Error posting data to server');
+        throw new Error("Error posting data to server");
       }
-    } catch(err) {
+      const createdComment = await response.json();
+      e.target.reset();
+      onNewComment(createdComment);
+    } catch (err) {
       console.error(err);
     }
   }
   return (
     <form className={styles.newCommentForm} onSubmit={handleNewComment}>
-      <textarea placeholder="Leave a comment!" name="newComment" id="newComment" rows={2} cols={40} />
+      <textarea
+        placeholder="Leave a comment!"
+        name="newComment"
+        id="newComment"
+        rows={2}
+        cols={40}
+      />
       <button type="submit">Add Comment</button>
     </form>
-  )
+  );
 }
 
 export default function Post() {
@@ -61,36 +76,38 @@ export default function Post() {
   const { user } = useOutletContext();
 
   useEffect(() => {
-      const controller = new AbortController();
-      const signal = controller.signal;
-      const jwt = localStorage.getItem("authToken");
-      const fetchPost = async () => {
-        try {
-          const response = await fetch(`http://localhost:8080/posts/${postId}`, { signal, 
-            headers: {
-              Authorization: `Bearer ${jwt}`,
-            }, });
-          if (!response.ok) {
-            throw new Error("Fetch error");
-          }
-          console.log(response);
-          const postData = await response.json();
-          console.log(postData);
-          setPost(postData)
-          setLoading(false);
-        } catch (err) {
-          if (err.name === "AbortError") {
-            console.log("Request aborted");
-          } else {
-            console.log(err);
-          }
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const jwt = localStorage.getItem("authToken");
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/posts/${postId}`, {
+          signal,
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Fetch error");
         }
-      };
-      fetchPost();
-      return () => controller.abort();
-    }, [postId]);
-    
- if (loading) {
+        console.log(response);
+        const postData = await response.json();
+        console.log(postData);
+        setPost(postData);
+        setLoading(false);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("Request aborted");
+        } else {
+          console.log(err);
+        }
+      }
+    };
+    fetchPost();
+    return () => controller.abort();
+  }, [postId]);
+
+  if (loading) {
     return <p>Loading...</p>;
   }
 
@@ -98,17 +115,34 @@ export default function Post() {
     return <p>Post not found.</p>;
   }
 
+  const onNewComment = (comment) => {
+    setPost((prevPost) => ({
+      ...prevPost,
+      comments: [...prevPost.comments, comment],
+    }));
+  };
+
   return (
     <div className={`post ${styles.postContainer}`}>
       <div className={`blogPost ${styles.post}`}>
         <h1 className={styles.title}>{post.title}</h1>
-        <div className={styles.content} dangerouslySetInnerHTML={{__html: post.message}}></div>
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: post.message }}
+        ></div>
       </div>
       <div className={styles.comments}>
-        {user ? <NewCommentForm postId={postId}/> : <>Login to add comments!</>} 
-        {post.comments.length < 1 ? <p className={styles.comment}>No comments yet.</p> : <Comments commentsData={post.comments}/>}
+        {user ? (
+          <NewCommentForm postId={postId} onNewComment={onNewComment} />
+        ) : (
+          <>Login to add comments!</>
+        )}
+        {post.comments.length < 1 ? (
+          <p className={styles.comment}>No comments yet.</p>
+        ) : (
+          <Comments commentsData={post.comments} />
+        )}
       </div>
-      
     </div>
   );
 }
